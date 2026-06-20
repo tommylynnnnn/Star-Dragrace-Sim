92a127916d7c57ba901f0f533d43ea2b876811be
tommylynnnnn <hildabaggin@gmail.com>
2026-06-20 16:28:36 -0400

    Delete displayQueensPool function from script.js
    Removed the displayQueensPool function and its invocation.

--- script.js
+++ script.js
@@ -23,32 +23,3 @@ const queens = [
 }
   
 ];
-
-
-// This function runs automatically to display your queens on the screen
-function displayQueensPool() {
-  // 1. Find the empty grid container in your HTML
-  const poolContainer = document.getElementById("queens-pool");
-  
-  // Clear it out just in case
-  poolContainer.innerHTML = "";
-
-  // 2. Loop through every queen in your array
-  queens.forEach((queen, index) => {
-    // 3. Create a piece of HTML for each queen card
-    const cardHtml = `
-      <div class="queen-card">
-        <img src="${queen.Image}" alt="${queen.Name}">
-        <h3>${queen.Name}</h3>
-        <p>Season ${queen.Season}</p>
-        <button onclick="addToCast(${index})">Add to Cast</button>
-      </div>
-    `;
-    
-    // 4. Drop that card into the container
-    poolContainer.innerHTML += cardHtml;
-  });
-}
-
-// 5. Call the function so it loads the moment the page opens!
-displayQueensPool();
