7e6d724222b4ab5e5c6bfc6b3f0c5bee4354e614
tommylynnnnn <hildabaggin@gmail.com>
2026-06-20 15:56:29 -0400

    Add queens data to script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const queens = [
 
+  {
+  Name: "Victoria 'Porkchop' Parker",
+  Acting: 0,
+  Comedy: 0,
+  Dance: 0,
+  Design: 1,
+  Lipsync: 1,
+  Talent: 0,
+  Season: 1
+  },
+  {
+  Name: "Tammie Brown",
+  Acting: 3,
+  Comedy: 1,
+  Dance: 1,
+  Design: 3,
+  Lipsync: 1,
+  Talent: 0,
+  Season: 1
+  }
+  
+];
+  
